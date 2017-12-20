aeb383b086296abb70faf98e3bda5c2e2062f7a2
Raghavendra <pradyumna.palutla@gmail.com>
2017-12-20 20:12:55 +0530

    My Project Setup Completed Sucessfully

---
+++ shoppingbackend/src/main/java/net/rag/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.rag.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
